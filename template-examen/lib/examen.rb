7cb1cf03cb9fb16d6cd98f8b3283f96f18e22313
Daniel Acero <edanielacero@gmail.com>
2020-06-16 10:14:47 -0400

    added spec file and added methods to lib folder/examen.rb

--- template-examen/lib/examen.rb
+++ template-examen/lib/examen.rb
@@ -1,5 +1,11 @@
 class examen
+    def inicializar
+        @montoinicial=0
+    end    
     def setMonto(monto)
         @monto=monto
     end
+    def getMonto(){
+        return @monto
+    }
 end    
